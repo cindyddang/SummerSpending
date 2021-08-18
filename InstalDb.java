25a1115e34ebfecb49d155078d554127e9eeaf2b
Duyen (Cindy) Dang <61917584+cindyddang@users.noreply.github.com>
2021-08-18 14:33:23 -0500

    Update dbName

--- InstalDb.java
+++ InstalDb.java
@@ -1,19 +1,12 @@
-/* Duyen Dang
-* July 30th, 2019
-* This class is for creating a new data and new table. */
 package money;
 
 import java.sql.Connection;
 
-/**
- *
- * @author DuyenDang
- */
 public class InstalDb
 {
     public static void main(String[] args)
     {
-        String dbName = "DuyenDang";
+        String dbName = "name";
         JavaDatabase objDb = new JavaDatabase();
         objDb.createDb(dbName);
         
